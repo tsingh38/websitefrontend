7c090bd120ab6eaca49c296afea159efd8671a90
Roxy <er.tajinder89@gmail.com>
2020-02-28 15:52:17 +0100

    Stuffe_1_fertig

--- src/app/shop/shop.component.ts
+++ src/app/shop/shop.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CartService } from '../services/cart.service';
 import { ActivatedRoute, Router, NavigationEnd } from '@angular/router';
 import { MealService } from '../services/mealservice';
-import { productItem,productItemAddition,productItemOption } from '../models/product.interface';
+import { productItem, productItemAddition, productItemOption } from '../models/product.interface';
 import { ShopTimingsUtil } from '../services/ShopTimingsUtil.service';
 
 @Component({
@@ -15,73 +15,140 @@ export class ShopComponent implements OnInit {
   private fragment: string;
 
   show = 3;
-  isShopAvailable:boolean;
+  isShopAvailable: boolean;
   allProductItems: productItem[] = [];
-  selectedCategory:string= "Beliebte";
+  selectedCategory: string = "Beliebte";
   productsForSelectedCategory: productItem[];
-  totalPriceInShoppingCart:string;
-  totalNumberOfItemsInCart:number;
+  totalPriceInShoppingCart: string;
+  totalNumberOfItemsInCart: number;
   searchFieldActivated = false;
-  categories: string[] = ["Beliebte", "Indische_Vorspeisen", "Salat", "Pizza", "Vegatarische_Pizza", "Calzone", "PizzabrotØ30", "Pasta","Pasta_al_Forno", "Indische_Gerichte",
+  categories: string[] = ["Beliebte", "Indische_Vorspeisen", "Salat", "Pizza", "Vegatarische_Pizza", "Calzone", "PizzabrotØ30", "Pasta", "Pasta_al_Forno", "Indische_Gerichte",
     "Gyros_Spezialitäten", "International_Gerichte", "Rösti", "Döner_kebab", "Lahmacun", "Vegatarische_Döner", "Pide", "Dessert",
     "BenAndJerrys", "Alkohalfrei_Getränke", "Alkohlische_Getränke"]
-    shopClosedHeaderMessage="Im Moment ist die Geschäft geschlossen";
-    shopClosedMessage="Die Bestellungen außerhalb unseren Geschäftszeiten können  nicht bearbeitet werden."
-  constructor(private mealService: MealService,private cartService:CartService,private router:Router) {
+  shopClosedHeaderMessage = "Im Moment ist die Geschäft geschlossen";
+  shopClosedMessage = "Die Bestellungen außerhalb unseren Geschäftszeiten können  nicht bearbeitet werden."
+  constructor(private mealService: MealService, private cartService: CartService, private router: Router) {
     this.allProductItems = this.mealService.fetchAllItems();
   }
 
   ngOnInit() {
-    this.isShopAvailable=ShopTimingsUtil.isShopOpenNow();
-    this.totalPriceInShoppingCart= this.getPriceWith2DecimalPlaces(this.cartService.totalOrderPrice);
-  if( !this.productsForSelectedCategory  || this.productsForSelectedCategory.length < 1){
-    this.productsForSelectedCategory=this.allProductItems;
-  }
-  this.cartService.totalOrderPriceEmitter.subscribe((param)=>{
-    this.totalPriceInShoppingCart=this.getPriceWith2DecimalPlaces(param.price);
-    this.totalNumberOfItemsInCart=param.quantity;
-  });
+    this.isShopAvailable = ShopTimingsUtil.isShopOpenNow();
+    this.totalPriceInShoppingCart = this.getPriceWith2DecimalPlaces(this.cartService.totalOrderPrice);
+    if (!this.productsForSelectedCategory || this.productsForSelectedCategory.length < 1) {
+      this.productsForSelectedCategory = this.allProductItems;
+    }
+    this.cartService.totalOrderPriceEmitter.subscribe((param) => {
+      this.totalPriceInShoppingCart = this.getPriceWith2DecimalPlaces(param.price);
+      this.totalNumberOfItemsInCart = param.quantity;
+    });
   }
 
 
-  getPriceWith2DecimalPlaces(price:Number){
-    return parseFloat(price+"").toFixed(2);
+  getPriceWith2DecimalPlaces(price: Number) {
+    return parseFloat(price + "").toFixed(2);
   }
-  getCategoryImageSource(){
-if(this.selectedCategory){
-  switch(this.selectedCategory){
- case "Indische_Vorspeisen" :return "../../assets/indischevorspeise.jpg";
- case "Salat" :return "../../assets/salat.jpg";
- case "Pizza" :return "../../assets/pizza.jpg";
- case "Vegatarische_Pizza" :return "../../assets/vegpizza.jpg";
- case "Calzone" :return "../../assets/calzone.jpg";
- case "PizzabrotØ30" :return "../../assets/pizzabrot.jpg";
- case "Pasta" :return "../../assets/pasta.jpg";
- case "Pasta_al_Forno" :return "../../assets/pasta.jpg";
- case "Indische_Gerichte" :return "../../assets/indischegerichte.jpg";
- case "Gyros_Spezialitäten" :return "../../assets/gyrosspezialitäten.jpg";
- case "International_Gerichte" :return "../../assets/international.jpg";
- case "Rösti" :return "../../assets/roesti.jpg";
- case "Döner_kebab" :return "../../assets/dönerkebab.jpg";
- case "Lahmacun" :return "../../assets/lahmacun.jpg";
- case "Vegatarische_Döner" :return "../../assets/dönerkebab.jpg";
- case "Pide" :return "../../assets/pide.jpg";
- case "Dessert" :return "../../assets/dessert.jpg";
- case "BenAndJerrys" :return "../../assets/benAndJerrys.jpg";
- case "Alkohalfrei_Getränke" :return "../../assets/alkohalfreiGetränke.jpeg";
- case "Alkohlische_Getränke" :return "../../assets/liqor.jpg";
+  getCategoryImageSource(index: number) {
+    if (this.selectedCategory) {
+      switch (this.selectedCategory) {
+        case "Indische_Vorspeisen":
+          switch (index) {
+            case 1: return "../../assets/indischevorspeise1.jpg";
+            case 2: return "../../assets/indischevorspeise2.jpg";
+          }
 
- default: return "../../assets/indischegerichte.jpg";
-}   
-}
+        case "Salat":
+          switch (index) {
+            case 1: return "../../assets/salat_1.jpg";
+            case 2: return "../../assets/salat_2.jpg";
+          }
+
+        case "Pizza": switch (index) {
+          case 1: return "../../assets/pizza_1.jpg";
+          case 2: return "../../assets/pizza_2.jpg";
+        }
+        case "Vegatarische_Pizza": switch (index) {
+          case 1: return "../../assets/vegpizza.jpg";
+          case 2: return "../../assets/pizza_2.jpg";
+        }
+        case "Calzone": switch (index) {
+          case 1: return "../../assets/calzone_1.jpg";
+          case 2: return "../../assets/calzone_2.jpg";
+        }
+        case "PizzabrotØ30": switch (index) {
+          case 1: return "../../assets/pizzabrot_1.jpg";
+          case 2: return "../../assets/pizzabrot_2.jpg";
+        }
+        case "Pasta": switch (index) {
+          case 1: return "../../assets/pasta_1.jpg";
+          case 2: return "../../assets/pasta_2.jpg";
+        }
+        case "Pasta_al_Forno": switch (index) {
+          case 1: return "../../assets/Pasta_al_Forno_1.jpg";
+          case 2: return "../../assets/Pasta_al_Forno_2.jpg";
+        }
+        case "Indische_Gerichte": switch (index) {
+          case 1: return "../../assets/indischegerichte_1.jpg";
+          case 2: return "../../assets/indischegerichte_2.jpg";
+        }
+        case "Gyros_Spezialitäten": switch (index) {
+          case 1: return "../../assets/gyrosspezialitäten_1.jpg";
+          case 2: return "../../assets/gyrosspezialitäten_2.jpg";
+        }
+        case "International_Gerichte": switch (index) {
+          case 1: return "../../assets/international_1.jpg";
+          case 2: return "../../assets/international_2.jpg";
+        }
+        case "Rösti": switch (index) {
+          case 1: return "../../assets/roesti_1.jpg";
+          case 2: return "../../assets/roesti_2.jpg";
+        }
+        case "Döner_kebab": switch (index) {
+          case 1: return "../../assets/dönerkebab_1.jpg";
+          case 2: return "../../assets/dönerkebab_2.jpg";
+        }
+        case "Lahmacun": switch (index) {
+          case 1: return "../../assets/lahmacun_1.jpg";
+          case 2: return "../../assets/lahmacun_2.jpg";
+        }
+        case "Vegatarische_Döner": switch (index) {
+          case 1: return "../../assets/dönerkebab_1.jpg";
+          case 2: return "../../assets/yufka.jpg";
+        }
+        case "Pide": switch (index) {
+          case 1: return "../../assets/pide_1.jpg";
+          case 2: return "../../assets/pide_2.jpg";
+        }
+        case "Dessert": switch (index) {
+          case 1: return "../../assets/dessert_1.jpg";
+          case 2: return "../../assets/dessert_2.jpg";
+        }
+        case "BenAndJerrys": switch (index) {
+          case 1: return "../../assets/benAndJerrys_1.jpg";
+          case 2: return "../../assets/benAndJerrys_2.jpg";
+        }
+        case "Alkohalfrei_Getränke": switch (index) {
+          case 1: return "../../assets/alkohalfreiGetränke.jpeg";
+          case 2: return "../../assets/alkohalfreiGetränke_2.jpeg";
+        }
+        case "Alkohlische_Getränke": switch (index) {
+          case 1: return "../../assets/liqor.jpg";
+          case 2: return "../../assets/liqor_2.jpg";
+        }
+        case "Beliebte": switch (index) {
+          case 1: return "../../assets/Pommes_Frites.jpg";
+          case 2: return "../../assets/Frühlingsrollen.jpg";
+        }
+        default: return "../../assets/indischegerichte.jpg";
+      }
+    }
   }
 
   mainCategorySelected(event: Event) {
     var selectedCategory: string = (<HTMLTextAreaElement>event.target).value;
-    this.selectedCategory=selectedCategory;
-    this.productsForSelectedCategory=[];
+    this.selectedCategory = selectedCategory;
+    this.productsForSelectedCategory = [];
     for (let currentProduct of this.allProductItems) {
-      if (currentProduct.productCategory === selectedCategory) { 
+      if (currentProduct.productCategory === selectedCategory) {
         this.productsForSelectedCategory.push(currentProduct);
       }
     }
@@ -89,22 +156,22 @@ if(this.selectedCategory){
   }
 
 
-  submitOrder(){
+  submitOrder() {
     this.router.navigate(['/cart']);
   }
 
-  onClose(){
-    this.isShopAvailable=true;
+  onClose() {
+    this.isShopAvailable = true;
   }
- 
-  
- 
-
- 
 
 
 
-  
 
- 
+
+
+
+
+
+
+
 }
