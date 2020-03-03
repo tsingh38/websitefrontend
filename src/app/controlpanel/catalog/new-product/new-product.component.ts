b529c79a236c9c078abdcf4bbe6b1f30d3e91cf4
Roxy <er.tajinder89@gmail.com>
2020-03-03 20:17:49 +0100

    'working'

--- src/app/controlpanel/catalog/new-product/new-product.component.ts
+++ src/app/controlpanel/catalog/new-product/new-product.component.ts
@@ -11,12 +11,28 @@ export class NewProductComponent implements OnInit {
   selectedCategory:string;
   articleDescription:string;
   isCategoryPizza:boolean;
+  option1PizzaDescription:string="klein, Ø26cm:'";
+  option2PizzaDescription:string="Groß, Ø30cm:";
+  option3PizzaDescription:string="Familie,46cm x 33cm:";
+  option4PizzaDescription:string="Party,60cm x 40cm:";
+  option1CalzoneDescription:string="klein, Ø26cm:";
+  option2CalzoneDescription:string="Groß, Ø30cm:";
+  hasProductOptions:string;
+  numberOfTimesOptionArray:number[];
+
+
+
   categories: string[] = ["Indische_Vorspeisen", "Salat", "Pizza", "Vegatarische_Pizza", "Calzone", "PizzabrotØ30", "Pasta", "Pasta_al_Forno", "Indische_Gerichte",
     "Gyros_Spezialitäten", "International_Gerichte", "Rösti", "Döner_kebab", "Lahmacun", "Vegatarische_Döner", "Pide", "Dessert",
     "BenAndJerrys", "Alkohalfrei_Getränke", "Alkohlische_Getränke"]
-  constructor() { }
+  constructor() { 
+
+    this.numberOfTimesOptionArray=Array();
+  }
 
   ngOnInit() {
   }
+
+
 
 }
